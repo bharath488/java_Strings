2ea5f3f20bbbfe36c9379268075cd3d6d222f06f
bharath488 <22hr1a05b2@mtieat.org>
2025-07-10 22:33:48 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class palindromeCheck{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a string:");
+String original=sc.nextLine();
+String reversed="";
+for(int i=original.length()-1;i>=0;i--){
+reversed+=original.charAt(i);
+}
+if(original.equalsIgnoreCase(reversed)){
+System.out.println(original+" is a palindrome");
+}
+else{
+System.out.println(reversed+" is not a palindrome");
+}
+}
+}
